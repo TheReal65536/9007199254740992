5c81c2c330bfc4a9e022792f412ec74be83fd01a
sirknighting <sirknightingfail@gmail.com>
2014-05-06 13:05:38 -0700

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -185,7 +185,9 @@ GameManager.prototype.move = function (direction) {
 
   if (moved) {
     this.addRandomTile();
-
+    this.addRandomTile();
+    this.addRandomTile();
+    this.addRandomTile();
     if (!this.movesAvailable()) {
       this.over = true; // Game over!
     }
